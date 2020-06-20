c79b5639b014efb82182efd1631cc7a10fded283
John Ferguson Smart <john.smart@wakaleo.com>
2020-06-20 11:18:36 +0100

    Refactored glue code

--- src/test/java/calculator/stepdefinitions/ShowHistoryStepDefinitions.java
+++ src/test/java/calculator/stepdefinitions/ShowHistoryStepDefinitions.java
@@ -1,8 +1,6 @@
 package calculator.stepdefinitions;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
 
 import java.util.List;
 import java.util.Map;
