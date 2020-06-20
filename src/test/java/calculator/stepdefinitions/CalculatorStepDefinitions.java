59facf42fa9931aba22d66f682354cfc3a8bdf62
John Ferguson Smart <john.smart@wakaleo.com>
2020-06-20 11:10:19 +0100

    Added glue code

--- src/test/java/calculator/stepdefinitions/CalculatorStepDefinitions.java
+++ src/test/java/calculator/stepdefinitions/CalculatorStepDefinitions.java
@@ -13,10 +13,10 @@ public class CalculatorStepDefinitions {
     public void matt_a_maths_student_has_opened_the_calculator() {
     }
 
-    @When("Matt performs the following calculation:")
-    public void matt_performs_the_following_calculation(List<Map<String, String>> operations) {
+        @When("^Matt (?:performs|has performed) the following calculations?:")
+        public void matt_performs_the_following_calculation(List<Map<String, String>> operations) {
 
-    }
+        }
 
     @Then("he should see a result of {string}")
     public void he_should_see_a_result_of(String expectedResult) {
---
+++ src/test/java/calculator/stepdefinitions/ShowHistoryStepDefinitions.java
@@ -0,0 +1,15 @@
+package calculator.stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import java.util.List;
+import java.util.Map;
+
+public class ShowHistoryStepDefinitions {
+
+    @Given("the calculation history should contain:")
+    public void matt_a_maths_student_has_opened_the_calculator(List<Map<String,String>> history) {
+    }
+}
